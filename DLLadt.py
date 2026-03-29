8e9353f010c9b6211209da64f1d588398c3f6a9e
Ayaan <mn10033@st.habib.edu.pk>
2026-03-30 00:55:24 +0500

    Added Doubly Linked List ADT(DLLadt.py) for music manager, tested multiple edge cases with context to our music manager problem to see if it works, all seems fine

---
+++ DLLadt.py
@@ -0,0 +1,208 @@
+"""
+
+  DOUBLY LINKED LIST ADT (DLL)
+  
+"""
+
+def create_node(data):
+    
+    node={
+        "prev":None,
+        "next":None,
+        "data":data   
+    }
+    
+    return node
+    
+
+def create_list():
+    
+    current={
+        "head":None,
+        "tail":None,
+        "size": 0 
+    }
+
+    return current
+
+
+def insertEnd(lst,data):
+    
+    
+    node=create_node(data)
+     
+    if lst["head"]==None: #if there are no nodes yet, we will assign head and tail to be the node that's being inserted
+        
+        lst["head"]=node
+        lst["tail"]=node
+        
+        
+    else: #else it advances tail and inserts the node at the end.
+       
+        node["prev"]=lst["tail"]
+        lst["tail"]["next"]=node
+        lst["tail"]=node
+        
+    lst["size"]+=1
+
+
+
+
+def insertStart(lst,data):
+    
+    
+    node=create_node(data)
+    
+    if lst["head"]==None: #if there are no nodes yet, we will assign head and tail to be the node that's being inserted
+        
+        lst["head"]=node
+        lst["tail"]=node
+        
+        
+    else: #else it advances head and inserts the node at the beginning.
+       
+        node["next"]=lst["head"]
+        lst["head"]["prev"]=node
+        lst["head"]=node
+        
+    lst["size"]+=1
+
+
+def insertMiddle(lst,data,target):
+    
+    
+    if target==lst["tail"]:
+        
+        insertEnd(lst,data)
+        return
+    
+    else:    
+        
+        node=create_node(data)
+        
+        node["next"]=target["next"]
+        node["prev"]=target
+        target["next"]["prev"]=node
+        target["next"]=node
+    
+    
+    lst["size"]+=1
+        
+    
+
+
+def delete_node(lst,node):
+    
+        
+    if node==None:
+        print("Nothing to delete")
+        return
+    
+    if lst["size"]==1:
+        lst["head"]=None
+        lst["tail"]=None
+    
+    if node==lst["tail"]:
+        
+        lst["tail"]=node["prev"]
+        lst["tail"]["next"]=None
+        
+    elif node==lst["head"]:
+        
+        lst["head"]=node["next"]
+        lst["head"]["prev"]=None
+        
+    else:
+
+        node["prev"]["next"]=node["next"]
+        node["next"]["prev"]=node["prev"]
+        
+    lst["size"]-=1
+    
+
+
+
+def traverse(lst):
+    
+    current=lst["head"]
+    
+    
+    if lst["size"]!=0:
+    
+        while True:
+            
+            print(current["data"])
+            
+            if current==lst["tail"]:
+                break
+            current=current["next"]
+        
+
+def traverseback(lst):
+    
+    current=lst["tail"]
+    
+    if lst["size"]!=0:
+    
+        while True:
+            
+            print(current["data"])
+            
+            if current==lst["head"]:
+                break
+            current=current["prev"]
+        
+
+
+def search(lst,key,value):
+    
+    current=lst["head"]
+    
+    if lst["size"]!=0:
+    
+        while True:
+            
+            if current["data"][key]==value:
+                return current
+        
+            if current==lst["tail"]:
+                break
+                
+            current=current["next"]
+            
+    return None
+
+
+
+
+# create a list and add 3 songs
+playlist = create_list()
+insertEnd(playlist, {"title": "What lurks in the Forest", "artist": "Akira Yamaoka"})
+insertEnd(playlist, {"title": "In my time of need", "artist": "Opeth"})
+insertEnd(playlist, {"title": "Opiate^2", "artist": "TOOL"})
+insertEnd(playlist, {"title": "Hope Leaves", "artist": "Opeth"})
+
+# should print A, B, C
+traverse(playlist)
+
+# should print C, B, A
+traverseback(playlist)
+
+# should return Song B's node
+result=search(playlist, "artist", "TOOL")
+
+print(result["data"])
+
+# delete Song B then traverse, should print A, C
+delete_node(playlist, result)
+
+result=search(playlist, "artist", "Opeth")
+delete_node(playlist, result)
+
+traverse(playlist)
+
+# size should be 2
+print(playlist["size"])
+
+
+
