57bd2a9020532dac8f1ec1264f9843872829383a
mustafasmh <sh07715@st.habib.edu.pk>
2026-03-31 15:00:23 +0500

    Add comments to DLLadt.py
    Added proper comments to DLLadt.py to have a heading comment
    which explains what function intends to do, as well as line-by-
    line comments to explain how the function works.

--- DLLadt.py
+++ DLLadt.py
@@ -4,93 +4,101 @@
   
 """
 
+# Function to create a node:
+# Each node is a dictionary 
 def create_node(data):
     
     node={
-        "prev":None,
-        "next":None,
-        "data":data   
+        "prev":None, # pointer to previous node
+        "next":None, # pointer to next node
+        "data":data  # data that is stored in the node
     }
     
-    return node
+    return node # node is returned after being created
     
 
+# Function to create an empty doubly linked list
 def create_list():
-    
+    # Initialize a dictionary to represent the doubly linked list
     current={
-        "head":None,
-        "tail":None,
-        "size": 0 
+        "head":None,      # pointer to the first node in the list
+        "tail":None,      # pointer to the last node in the list
+        "size": 0         # counter to track the number of nodes in the list
     }
 
-    return current
+    return current  # return the initialized list structure
 
 
+# Function to insert a new node at the end of the list
 def insertEnd(lst,data):
     
     
     node=create_node(data)
      
-    if lst["head"]==None: #if there are no nodes yet, we will assign head and tail to be the node that's being inserted
+    if lst["head"]==None: # if there are no nodes yet, assign head and tail to this new node
         
         lst["head"]=node
         lst["tail"]=node
         
         
-    else: #else it advances tail and inserts the node at the end.
+    else: # otherwise append after current tail and update the tail pointer
        
         node["prev"]=lst["tail"]
         lst["tail"]["next"]=node
         lst["tail"]=node
         
-    lst["size"]+=1
+    lst["size"]+=1 # increment size after insertion
 
 
 
 
+# Function to insert a new node at the start of the list
 def insertStart(lst,data):
     
     
     node=create_node(data)
     
-    if lst["head"]==None: #if there are no nodes yet, we will assign head and tail to be the node that's being inserted
+    if lst["head"]==None: # if list is empty, new node becomes both head and tail
         
         lst["head"]=node
         lst["tail"]=node
         
         
-    else: #else it advances head and inserts the node at the beginning.
+    else: # otherwise link new node before current head and update head pointer
        
         node["next"]=lst["head"]
         lst["head"]["prev"]=node
         lst["head"]=node
         
-    lst["size"]+=1
+    lst["size"]+=1 # increment size after insertion
 
 
+# Function to insert a new node after a specified target node
 def insertMiddle(lst,data,target):
     
     
     if target==lst["tail"]:
         
-        insertEnd(lst,data)
+        insertEnd(lst,data) # insert at end when target is tail
         return
     
     else:    
         
         node=create_node(data)
         
-        node["next"]=target["next"]
-        node["prev"]=target
-        target["next"]["prev"]=node
-        target["next"]=node
+        node["next"]=target["next"] # connect new node to target's successor
+        node["prev"]=target # set previous pointer to target
+        target["next"]["prev"]=node # update successor to point back to new node
+        target["next"]=node # insert new node after target
     
     
-    lst["size"]+=1
+    lst["size"]+=1 # update size after insertion
         
     
 
 
+
+# Function to delete a node from the list
 def delete_node(lst,node):
     
         
@@ -98,30 +106,31 @@ def delete_node(lst,node):
         print("Nothing to delete")
         return
     
-    if lst["size"]==1:
+    if lst["size"]==1: # if list has one node, clear the list
         lst["head"]=None
         lst["tail"]=None
     
-    if node==lst["tail"]:
+    if node==lst["tail"]: # removing the last node
         
         lst["tail"]=node["prev"]
         lst["tail"]["next"]=None
         
-    elif node==lst["head"]:
+    elif node==lst["head"]: # removing the first node
         
         lst["head"]=node["next"]
         lst["head"]["prev"]=None
         
-    else:
+    else: # removing a node from the middle
 
         node["prev"]["next"]=node["next"]
         node["next"]["prev"]=node["prev"]
         
-    lst["size"]-=1
+    lst["size"]-=1 # decrement size after deletion
     
 
 
 
+# Function to traverse the list from head to tail
 def traverse(lst):
     
     current=lst["head"]
@@ -131,13 +140,14 @@ def traverse(lst):
     
         while True:
             
-            print(current["data"])
+            print(current["data"]) # print current node data
             
             if current==lst["tail"]:
                 break
-            current=current["next"]
+            current=current["next"] # move to next node in forward direction
         
 
+# Function to traverse the list from tail to head
 def traverseback(lst):
     
     current=lst["tail"]
@@ -146,14 +156,15 @@ def traverseback(lst):
     
         while True:
             
-            print(current["data"])
+            print(current["data"]) # print current node data in reverse order
             
             if current==lst["head"]:
                 break
-            current=current["prev"]
+            current=current["prev"] # move to previous node in backward direction
         
 
 
+# Function to search for a node by key/value
 def search(lst,key,value):
     
     current=lst["head"]
@@ -163,12 +174,12 @@ def search(lst,key,value):
         while True:
             
             if current["data"][key]==value:
-                return current
+                return current # return node when key/value pair matches
         
             if current==lst["tail"]:
                 break
                 
-            current=current["next"]
+            current=current["next"] # move to next node to continue searching
             
     return None
 
