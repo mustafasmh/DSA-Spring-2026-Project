68dd3a7bbc2e160476ddc8bc8c5fd67c3a111904
Ayaan <mn10033@st.habib.edu.pk>
2026-04-02 00:31:21 +0500

    I've written the artist list function that should display the specfic songs of the artists, I have removed your comments for the time being as they made it less readable for me, I will readd them or write my own soon. I have also adjusted spacing in the code you've written to make it more readable, good work on the logic itself, looks nice.

--- playlist_adt.py
+++ playlist_adt.py
@@ -1,37 +1,56 @@
-# Pointlist - Music Playlist Manager
-# Team: Ayaan Niazi, Tooba Zehra, Sayed Mustafa Hussain
+"""
 
+ Pointlist - Music Playlist Manager
+ Team: Ayaan Niazi, Tooba Zehra, Sayed Mustafa Hussain
+
+"""
 from DLLadt import *
 
+
 # Each song is a node in our playlist
-def create_song(title, artist, duration, genre):
+
+
+def create_song(title,artist,duration,genre):
     
     data = {
-        "title" : title,
-        "artist": artist,
-        "duration": duration,
-        "genre": genre
+        "title":title,
+        "artist":artist,
+        "duration":duration,
+        "genre":genre
         }
     
     song = create_node(data)
     return song
 
 # A playlist manager needs to have the ability to create a new empty playlist
+
+
 def create_playlist():
-    playlist = create_list()
+    
+    playlist=create_list()
 
     return playlist
 
 # A playlist manager needs to have the ability to add a song:
+
+
 def add_song(playlist, title, artist, duration, genre):
-    new_song = create_song(title, artist, duration, genre)
+    
+    new_song=create_song(title, artist, duration, genre)
     insertEnd(playlist, new_song["data"])
+    node=playlist["tail"]
+    artist_list(playlist, node)
 
 # A playlist manager needs to have the ability to delete songs:
+
+
 def remove_song(playlist, title):
+    
+    
     node = search(playlist, "title", title)
 
     if node is None:
+        
         print("Song not found")
         return
 
@@ -39,8 +58,13 @@ def remove_song(playlist, title):
     print("Song removed successfully")
 
 # A playlist manager needs to have the ability to display a playlist:
+
+
 def display_playlist(playlist):
+    
+    
     if playlist["size"] == 0:
+        
         print("Playlist is empty")
         return
 
@@ -48,9 +72,11 @@ def display_playlist(playlist):
     number = 1
 
     while True:
+        
         song = current["data"]
         mins = song["duration"] // 60 #
         secs = song["duration"] % 60
+        
         print(f"{number}. {song["title"]} - {song["artist"]} | {song["genre"]} | {mins}:{secs:02d}") 
 
         number += 1
@@ -62,4 +88,52 @@ def display_playlist(playlist):
 
 
 
+def artist_list(playlist,node):
+    
+    name=node["data"]["artist"]
+    
+    if name in playlist["artist_heads"]:
+        
+        playlist["artist_tails"][name]["artist_next"]=node
+        playlist["artist_tails"][name]=node 
+        
+    else:
+        
+        playlist["artist_heads"][name]=node
+        playlist["artist_tails"][name]=node
+ 
 
+
+
+def get_artist_songs(playlist,artist):
+    
+    if artist not in playlist["artist_heads"]:
+        
+        print("Artist not found")
+        return
+    
+    current=playlist["artist_heads"][artist]
+    
+    number=1
+
+    while True:
+        
+        song=current["data"]
+        mins=song["duration"]//60
+        secs=song["duration"]%60
+        
+        print(f"{number}. {song["title"]} - {song["artist"]} | {song["genre"]} | {mins}:{secs:02d}") 
+
+        number+=1
+        if current==playlist["artist_tails"][artist]:
+            break
+        current=current["artist_next"]
+
+
+
+    
+    
+    
+    
+    
+    
--- test_playlist.py
+++ test_playlist.py
@@ -6,8 +6,14 @@ playlist = create_playlist()
 add_song(playlist, "Blinding Lights", "The Weeknd", 200, "Pop")
 add_song(playlist, "Bohemian Rhapsody", "Queen", 354, "Rock")
 add_song(playlist, "Shape of You", "Ed Sheeran", 234, "Pop")
+add_song(playlist, "Starboy", "The Weeknd", 230, "Pop")
+add_song(playlist, "Since I've Been Loving you", "Led Zeppelin", 676, "Rock")
 
-display_playlist(playlist)
+
+# display_playlist(playlist)
+
+get_artist_songs(playlist,"The Weeknd")
+
 
 '''
 Expected output:
